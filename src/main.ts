0ce3f9a98fa007c9f4b888d28989c1ba0cdc8c65
ashu <utkarsh.bisht@block8.io>
2020-08-21 19:16:38 +0530

    Swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from './app.module';
 import { NestFastifyApplication, FastifyAdapter } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const options = new DocumentBuilder()
+    .setTitle('NestJs base demo')
+    .setDescription('Users and notes API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
